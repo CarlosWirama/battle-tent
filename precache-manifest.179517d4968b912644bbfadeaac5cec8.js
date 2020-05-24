9bb66a21d010765150e06b9cb1d415f91d35d93d
CarlosWirama <carloswirama@gmail.com>
2020-05-24 20:59:43 +0800

    Updates

---
+++ precache-manifest.179517d4968b912644bbfadeaac5cec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86208e123319422c6e730b54715bbf0d",
+    "url": "/battle-tent/index.html"
+  },
+  {
+    "revision": "c75c4f677448885f1e7f",
+    "url": "/battle-tent/static/css/main.886bcbed.chunk.css"
+  },
+  {
+    "revision": "4dcfe104964af45687af",
+    "url": "/battle-tent/static/js/2.5222e4b6.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/battle-tent/static/js/2.5222e4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75c4f677448885f1e7f",
+    "url": "/battle-tent/static/js/main.1002adb2.chunk.js"
+  },
+  {
+    "revision": "0b2d85423e032f2f068d",
+    "url": "/battle-tent/static/js/runtime-main.1957e468.js"
+  }
+]);
